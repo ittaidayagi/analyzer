4d0cb5ab139001ba9eb42fe205f002a2adfdb4f4
ittaidayagi <62358677+ittaidayagi@users.noreply.github.com>
2020-09-14 10:54:06 +0300

    Add close file and send file

--- venv/src/copy_file.py
+++ venv/src/copy_file.py
@@ -6,58 +6,89 @@ import shutil
 
 class files_utils():
 
-	def __init__(self, file_path, shadow_copy):
+	def __init__(self):
 
-		self.file_path = file_path
-		self.shadow_copy = shadow_copy
 		self.default_config = {
-			"WMI_Query": "SELECT * FROM Win32_ShadowCopy where ID='{0}'".format(vss_id),
+			"WMI_Query": "SELECT * FROM Win32_ShadowCopy where ID='{0}'",
 			"WMI_Shadow_Copy": "winmgmts:\\\\.\\root\\cimv2:Win32_ShadowCopy"
 		}
+		self.opened = False
 
 
-	def get(self, key):
+	def __get(self, key):
 
 		return self.default_config[key]
 
-	def get_vss_by_id(self, vss_id):
+	def __get_vss_by_id(self, vss_id):
 		wcd = win32com.client.Dispatch("WbemScripting.SWbemLocator")
 		wmi = wcd.ConnectServer(".","root\cimv2")
-		obj = wmi.ExecQuery(self.get("WMI_Query"))
+		obj = wmi.ExecQuery(self.__get("WMI_Query").format(vss_id))
 		return [x.DeviceObject for x in obj]
 
 	def open(self):
 
+		if self.opened:
+			raise "The shadow copy already open"
 		local_drives = get_local_drives()
 		vss = dict()
 		for local_drive in local_drives:
 
-			vss[local_drive] = self.create_shadow_copy(local_drive)
+			vss[local_drive] = self.__create_shadow_copy(local_drive)
 		self.vss = vss
+		self.opened = True
 
-	def create_shadow_copy(self,drive_letter):
+	def __create_shadow_copy(self,drive_letter):
 
-		wmi = win32com.client.GetObject(self.get("WMI_Shadow_Copy"))
+		wmi = win32com.client.GetObject(self.__get("WMI_Shadow_Copy"))
 		createmethod = wmi.Methods_("Create")
 		createparams = createmethod.InParameters
 		createparams.Properties_[1].value = "{0}:\\".format(drive_letter)
 		results = wmi.ExecMethod_("Create", createparams)
 		return results.Properties_[1].value
 
-	def copy_file(self, source, destination):
+	def __get_shadow_path(self, source):
 
 		splited_source = source.split(":\\")
 		drive = splited_source[0].upper()
 		if not drive in self.vss.keys():
-
 			raise "Unknown local drive"
 
 		path = splited_source[1]
-		shadow_path = self.get_vss_by_id(self.vss[drive])
-		full_path = os.path.join(shadow_path,path)
-		try: shutil.copyfile(full_path,destination)
+		shadow_path = self.__get_vss_by_id(self.vss[drive])
+		full_path = os.path.join(shadow_path, path)
+		return full_path
+
+	def copy_file(self, source, destination):
+
+		path = self.__get_shadow_path(source)
+		try: shutil.copyfile(path,destination)
 		except: return 1
 		else: return 0
+
+	def send_file(self, source, destination):
+
+		path = self.get_shadow_path(source)
+		# TODO: a module for sending with socket than use it here
+
+	def __vss_delete(self, shadow_id):
+		wcd = win32com.client.Dispatch("WbemScripting.SWbemLocator")
+		wmi = wcd.ConnectServer(".", "root\cimv2")
+		obj = wmi.ExecQuery(
+			self.__get("WMI_Query").format(
+				shadow_id))
+		try: obj[0].Delete_()
+		except: return 1
+		else: return 0
+
+	def close(self):
+
+		if not self.opened:
+			raise "There is no open shadow copy"
+		result = 0
+		for drive in self.vss.values():
+			result = self.__vss_delete(drive) & result
+		self.opened = False
+		return 0
 
 def get_local_drives():
 
