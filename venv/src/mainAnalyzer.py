a1db18faaf027484f7fc0142a8d1fd87fe284a92
ittaidayagi <62358677+ittaidayagi@users.noreply.github.com>
2020-09-15 09:45:09 +0300

    Add documentations

--- venv/src/mainAnalyzer.py
+++ venv/src/mainAnalyzer.py
@@ -1,8 +1,10 @@
+# Imports
 import sys
 import argparse
 import json
 import os
 
+# Contants
 PLUGINS_MODULES = r'c:\users\ittai\pycharmprojects\analyzer\venv\src\plugins_modules'
 CONFIG_FILE = r"c:\users\ittai\pycharmprojects\analyzer\venv\src\config"
 
@@ -16,25 +18,43 @@ class analyzer:
         self.start_analyzer()
 
     def start_analyzer(self):
+        """
+        Starting the analyzer and run all of the plugins
+        :return: None
+        """
 
+        # Change directory to the plugins directory
         sys.path.insert(1, PLUGINS_MODULES)
+
+        # Import all the plugins
         for module_name in filter(lambda x: x.endswith("py"), os.listdir(PLUGINS_MODULES)):
             exec ("import {0}".format(module_name[:-3]))
 
+        # Run all the plugins the user activate
         for plugin in self.plugins:
 
              if plugin:
                  exec("{0}.{0}()".format(plugin))
 
 def get_plugins_by_config(config_file, parser):
+    """
+    Get all the plugins from the config file and add arguemnts for every plugin
+    :param config_file: The config file
+    :param parser: The argument parser object
+    :return: None
+    """
 
+    # Read the content of the config file
     with open(CONFIG_FILE, "r") as config_file:
         config_raw = config_file.read()
 
+    # Convert the content of the config file to json
     config_json = json.loads(config_raw)
 
+    # Get all the plugins from the config file
     plugins = config_json["plugins"].keys()
 
+    # For every plugin, add argument to the parser
     for plugin in plugins:
 
         plugin_json = config_json["plugins"][plugin]
@@ -42,17 +62,22 @@ def get_plugins_by_config(config_file, parser):
 
 
 def main():
+
+    # Create argument parser and add destination and path arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-d", "--destination")
     parser.add_argument("-p", "--path")
 
+    # Create arguments from the config file
     get_plugins_by_config(CONFIG_FILE,parser)
 
+    # Get the arguments from the config file
     args = vars(parser.parse_args())
     destination = args.pop("destination")
     path = args.pop("path")
     plugins = args
 
+    # Run analyzer
     analyzer(destination,path,plugins)
 
 if __name__ == "__main__":
--- venv/src/setup.py
+++ venv/src/setup.py
@@ -1,3 +1,7 @@
 from distutils.core import setup
-import py2exe
-setup(console=['mainAnalyzer.py'])
+import py2exe, sys, os
+sys.argv.append('py2exe')
+setup(
+    options = {'py2exe': {'bundle_files': 0}},
+    windows = [{'script': "mainAnalyzer.py"}],
+    zipfile = None,)
